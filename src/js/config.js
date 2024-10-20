a1662f2dfd38a826cee71a3ea0a12b9c2e7932cb
nikolasladic <nikola.sladic5@gmail.com>
2024-10-20 15:51:04 +0200

    first commit

---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = 'e24f571d-d6f1-4028-9007-4a2aeb8cd57c';
+export const MODAL_CLOSE_SEC = 2.5;
